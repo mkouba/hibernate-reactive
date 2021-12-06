71d6943483cdbb2f640fee5bbdcb94209e8484ff
blafond <blafond@redhat.com>
2021-12-06 12:51:40 -0600

    Added Oracle extraction support for Indexes and Imported Keys

--- hibernate-reactive-core/src/main/java/org/hibernate/reactive/provider/service/OracleSqlReactiveInformationExtractorImpl.java
+++ hibernate-reactive-core/src/main/java/org/hibernate/reactive/provider/service/OracleSqlReactiveInformationExtractorImpl.java
@@ -26,7 +26,7 @@ public class OracleSqlReactiveInformationExtractorImpl extends AbstractReactiveI
 
 	@Override
 	protected String getResultSetTableTypesPhysicalTableConstant() {
-		return "BASE TABLE";
+		return "TABLE";
 	}
 
 	@Override
@@ -47,50 +47,23 @@ public class OracleSqlReactiveInformationExtractorImpl extends AbstractReactiveI
 			boolean unique,
 			boolean approximate,
 			ExtractionContext.ResultSetProcessor<T> processor) throws SQLException {
-		// This functionality is not used by ORM.
-		throw new NotYetImplementedException();
-//		// This implementation is based on org.postgresql.jdbc.PgDatabaseMetaData#getIndexInfo.
-//		// It excludes columns that are specified by DatabaseMetaData#getIndexInfo, but
-//		// not specified by AbstractInformationExtractorImpl#processIndexInfoResultSet.
-//
-//		// TODO: How should the "approximate" parameter be used?
-//		// org.postgresql.jdbc.PgDatabaseMetaData#getIndexInfo
-//		// does not use that argument. It is currently ignored here as well.
-//
-//		// Generate the inner query first.
-//		final StringBuilder innerQuery = new StringBuilder()
-//				.append( "select ci.relname as index_name" )
-//				.append( " , case i.indisclustered when true then " ).append( DatabaseMetaData.tableIndexClustered )
-//				.append( " else case am.amname when 'hash' then " ).append( DatabaseMetaData.tableIndexHashed )
-//				.append( " else " ).append( DatabaseMetaData.tableIndexOther ).append( " end" )
-//				.append( " end as index_type" )
-//				.append( " , (information_schema._pg_expandarray(i.indkey)).n as position" )
-//				.append( " , ci.oid as ci_iod" )
-//				.append( " from pg_catalog.pg_class ct" )
-//				.append( " join pg_catalog.pg_namespace n on (ct.relnamespace = n.oid)" )
-//				.append( " join pg_catalog.pg_index i on (ct.oid = i.indrelid)" )
-//				.append( " join pg_catalog.pg_class ci on (ci.oid = i.indexrelid)" )
-//				.append( " join pg_catalog.pg_am am on (ci.relam = am.oid)" )
-//				.append( " where true" );
-//
-//		final List<Object> parameterValues = new ArrayList<>();
-//
-//		appendClauseAndParameterIfNotNullOrEmpty( " and n.nspname = ", schema, innerQuery, parameterValues );
-//		appendClauseAndParameterIfNotNullOrEmpty( " and ct.relname = ", table, innerQuery, parameterValues );
-//
-//		if ( unique ) {
-//			innerQuery.append( " AND i.indisunique = true" );
-//		}
-//
-//		return getExtractionContext().getQueryResults(
-//				"select tmp.index_name as " + getResultSetIndexNameLabel() +
-//						", tmp.index_type as " + getResultSetIndexTypeLabel() +
-//						", trim(both '\"' from pg_catalog.pg_get_indexdef(tmp.ci_iod, tmp.position, false)) as " + getResultSetColumnNameLabel() +
-//						" from ( " + innerQuery + " ) tmp" +
-//						" order by " + getResultSetIndexNameLabel() + ", tmp.position",
-//				parameterValues.toArray(),
-//				processor
-//		);
+
+		final StringBuilder sb = new StringBuilder()
+				.append("select uic.index_name as " ).append( getResultSetIndexNameLabel() )
+				.append( ", -1 as " ).append( getResultSetIndexTypeLabel() )
+				.append( ", uic.column_name as " ).append( getResultSetColumnNameLabel() )
+				.append( " from user_ind_columns uic join user_indexes ui on ui.table_name = uic.table_name" )
+				.append( " where 1 = 1");
+
+		final List<Object> parameters = new ArrayList<>();
+		// Oracle's index TABLE_OWNER (schema) name and TABLE_NAME in USER_INDEXES
+		// column names for an index are in USE_IND_COLUMNS
+		assert catalog == null || catalog.isEmpty();
+		appendClauseAndParameterIfNotNullOrEmpty( " and ui.table_owner = ", schema, sb, parameters );
+		appendClauseAndParameterIfNotNullOrEmpty( " and ui.table_name = ", table, sb, parameters );
+
+
+		return getExtractionContext().getQueryResults( sb.toString(), parameters.toArray(), processor );
 	}
 
 	@Override
@@ -100,37 +73,42 @@ public class OracleSqlReactiveInformationExtractorImpl extends AbstractReactiveI
 			String table,
 			ExtractionContext.ResultSetProcessor<T> processor
 	) throws SQLException {
-		// This functionality is not used by ORM.
-		throw new NotYetImplementedException();
-		// This implementation is based on org.postgresql.jdbc.PgDatabaseMetaData#getImportedExportedKeys.
-		// It excludes columns that are specified by DatabaseMetaData#getImportedKeys, but
-		// not specified by AbstractInformationExtractorImpl#processImportedKeysResultSet.
 
-//		final StringBuilder sb = new StringBuilder()
-//				.append( "select null as " ).append( getResultSetPrimaryKeyCatalogLabel() )
-//				.append( ", pkn.nspname as " ).append( getResultSetPrimaryKeySchemaLabel() )
-//				.append( ", pkc.relname as " ).append( getResultSetPrimaryKeyTableLabel() )
-//				.append( ", pka.attname as " ).append( getResultSetPrimaryKeyColumnNameLabel() )
-//				.append( ", fka.attname as " ).append( getResultSetForeignKeyColumnNameLabel() )
-//				.append( ", pos.n as " ).append( getResultSetColumnPositionColumn() )
-//				.append( ", con.conname as " ).append( getResultSetForeignKeyLabel() )
-//				.append( " from pg_catalog.pg_namespace pkn, pg_catalog.pg_class pkc, pg_catalog.pg_attribute pka" )
-//				.append( ",  pg_catalog.pg_namespace fkn, pg_catalog.pg_class fkc, pg_catalog.pg_attribute fka" )
-//				.append( ", pg_catalog.pg_constraint con" )
-//				.append( ", pg_catalog.generate_series(1, cast( (select setting from pg_catalog.pg_settings where name='max_index_keys') as integer ) ) pos(n)" )
-//				.append( " where pkn.oid = pkc.relnamespace and pkc.oid = pka.attrelid and pka.attnum = con.confkey[pos.n] and con.confrelid = pkc.oid" )
-//				.append( " and fkn.oid = fkc.relnamespace and fkc.oid = fka.attrelid and fka.attnum = con.conkey[pos.n] and con.conrelid = fkc.oid" )
-//				.append( " and con.contype = 'f' " );
-//
-//		final List<Object> parameterValues = new ArrayList<>();
-//
-//		appendClauseAndParameterIfNotNullOrEmpty( " and fkn.nspname = ", schema, sb, parameterValues );
-//		appendClauseAndParameterIfNotNullOrEmpty( " and fkc.relname = ", table, sb, parameterValues );
-//
-//		// No need to order by catalog since it is always null.
-//		sb.append( " order by pkn.nspname, pkc.relname, con.conname, pos.n" );
-//
-//		return getExtractionContext().getQueryResults( sb.toString(), parameterValues.toArray(), processor );
+		String fkNameSubquery = "(select constraint_name from user_constraints where r_owner = '" + schema + "' and table_name = '" + table + "')";
+		String constraintSubquery =
+				"(SELECT R_CONSTRAINT_NAME FROM USER_CONSTRAINTS WHERE OWNER = '" + schema + "' and table_name = '" + table + "' and CONSTRAINT_TYPE = 'R')";
+
+		final StringBuilder sb = new StringBuilder()
+				.append( "select " + fkNameSubquery + " as " ).append( getResultSetForeignKeyLabel() )
+				.append( ", null as " ).append( getResultSetPrimaryKeyCatalogLabel() )
+				.append( ", uc.owner as " ).append( getResultSetPrimaryKeySchemaLabel() )
+				.append( ", ucc.table_name as " ).append( getResultSetPrimaryKeyTableLabel() )
+				.append( ", ucc.column_name as ").append( getResultSetPrimaryKeyColumnNameLabel() )
+				.append( ", ucc.column_name as " ).append( getResultSetForeignKeyColumnNameLabel() )
+				.append( " from user_constraints uc join user_cons_columns ucc on uc.constraint_name = ucc.constraint_name ")
+				// Exclude primary keys, which do not have a referenced table.
+				.append( " where uc.constraint_name = " + constraintSubquery );
+
+
+		// Now add constraints for the requested catalog/schema/table
+
+		final List<Object> parameters = new ArrayList<>();
+
+		final List<String> orderByList = new ArrayList<>();
+		orderByList.add( "uc.owner" );
+		orderByList.add( "ucc.table_name" );
+		orderByList.add( "ucc.position" );
+
+		if ( orderByList.size() > 0 ) {
+			sb.append( " order by " ).append( orderByList.get( 0 ) );
+			for ( int i = 1 ; i < orderByList.size() ; i++ ) {
+				sb.append( ", " ).append( orderByList.get( i ) );
+			}
+		}
+
+		T result =  getExtractionContext().getQueryResults( sb.toString(), parameters.toArray(), processor );
+
+		return result;
 	}
 
 	@Override
@@ -157,7 +135,7 @@ public class OracleSqlReactiveInformationExtractorImpl extends AbstractReactiveI
 				.append( " , satc.comments as " ).append( getResultSetRemarksLabel() )
 				// Hibernate ORM does not currently do anything with remarks,
 				// so just return null for now.
-				.append( " from sys.all_tables sat, sys.all_tab_comments satc where 1 = 1" );
+				.append( " from sys.all_tables sat join sys.all_tab_comments satc on sat.TABLE_NAME = satc.TABLE_NAME where 1 = 1" );
 		List<Object> parameterValues = new ArrayList<>();
 		appendClauseAndParameterIfNotNullOrEmpty( " and " + schemaColumn + " like ", schemaPattern, sb, parameterValues );
 		appendClauseAndParameterIfNotNullOrEmpty( " and sat.table_name like ", tableNamePattern, sb, parameterValues );
